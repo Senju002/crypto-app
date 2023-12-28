cb66560e544c4f9929ed0a566b2e37ac09ab0573
Senju002 <rajkaran.mei34@gmail.com>
2023-12-28 16:39:32 +0700

    data for homepage and cryptocurrencies page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,13 @@ import millify from "millify";
 import { Col, Row, Statistic, Typography } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
 
@@ -49,6 +51,24 @@ const Homepage = () => {
             />
           </Col>
         </Row>
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">
+            Top 10 Cryptocurrencies in the world
+          </Title>
+          <Title level={3} className="show-more">
+            <Link to="/cryptocurrencies">Show More</Link>
+          </Title>
+        </div>
+        <Cryptocurrencies simplified />
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">
+            Latest Crypto News
+          </Title>
+          <Title level={3} className="show-more">
+            <Link to="/news">Show More</Link>
+          </Title>
+        </div>
+        <News />
       </Title>
     </>
   );
